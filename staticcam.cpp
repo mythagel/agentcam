9a7c54b2848308c79e0089904f267cd7b26e1c69
Nicholas Gill <mythagel@gmail.com>
2018-11-05 16:40:14 +1100

    Old WIP

--- staticcam.cpp
+++ staticcam.cpp
@@ -134,201 +134,8 @@ void output_path(const cl::Paths& paths) {
         }
         std::cout << "L " << r6(first.x) << " " << r6(first.y) << " ";
     }
+    std::cout << "\n";
 }
-
-struct state {
-    point_2 current;
-    double current_theta = -1;
-
-    double tool_radius;
-    double target_engagement;
-    cl::Paths stock;
-    cl::Paths model;
-
-
-    struct candidate_t {
-        double theta = 0;
-        double r = 0;
-
-        // Calculated
-        cl::Paths tool;
-        double engagement = -1;
-
-        point_2 generate_point(const point_2& current) const {
-            point_2 center;
-            center.x = current.x + (std::cos(theta) * r);
-            center.y = current.y + (std::sin(theta) * r);
-            return center;
-        }
-
-        void generate_tool(const point_2& current, double tool_radius) {
-            tool.clear();
-            tool.emplace_back();
-            auto& path = tool.back();
-
-            const unsigned segments = 128;
-            path.reserve(segments);
-            
-            auto center = generate_point(current);
-
-            double delta_theta = (2.0*PI) / segments;
-            double theta = 0;
-            for (unsigned i = 0; i < segments; ++i, theta += delta_theta) {
-                point_2 p;
-                p.x = center.x + (std::cos(theta)*tool_radius);
-                p.y = center.y + (std::sin(theta)*tool_radius);
-                path.push_back(scale_point(p));
-            }
-        }
-
-        void generate_engagement(const point_2& current, double tool_radius, const cl::Paths& stock) {
-            generate_tool(current, tool_radius);
-
-            cl::Clipper clipper;
-            clipper.AddPaths(stock, cl::ptClip, true);
-            clipper.AddPaths(tool, cl::ptSubject, false);
-
-            cl::PolyTree pt;
-            clipper.Execute(cl::ctIntersection, pt);
-
-            auto path_length = [](const cl::Paths& paths) {
-                double length = 0;
-                if (paths.empty()) return length;
-                for (auto& path : paths) {
-                    if (path.empty()) return length;
-                    auto p0 = unscale_point(path.front());
-                    for (auto& point : path) {
-                        auto p1 = unscale_point(point);
-                        length += std::sqrt(std::pow(p1.x - p0.x, 2) + std::pow(p1.y - p0.y, 2));
-                        p0 = p1;
-                    }
-                }
-                return length;
-            };
-
-            auto l0 = path_length(tool);
-            cl::Paths open;
-            OpenPathsFromPolyTree(pt, open);
-            if (!open.empty()) {
-                auto l1 = path_length(open);
-                engagement = 360.0 * (l1 / l0);
-            } else {
-                engagement = -1;
-            }
-        }
-    };
-
-    bool next(point_2& next_point) {
-        /* generate candidate rays (theta / radius)
-         * - potential next point is ray end point
-         * - ugly brute force approach
-         *
-         * calculate tool engagement for each ray (end point)
-         * if any tool point intersects the model, reduce radius until
-         * it does not (or remove potential point if not possible)
-         *
-         * increase radius while cutter engagement remains (approximately) constant
-         *
-         * classify potential cut as climb, conventional, or slot
-         * - reject conventional, slot sorted last
-         *
-         * sort closeness to current vector direction (prefer smooth movement)
-         */
-
-        // create candidate rays
-        std::vector<candidate_t> candidates;
-        unsigned rays = 16;
-        double delta_theta = (2*PI) / rays;
-        double theta = 0;
-        for (unsigned ray = 0; ray < rays; ++ray) {
-            candidate_t ci;
-            ci.theta = theta;
-            ci.r = 0.3;
-            candidates.push_back(ci);
-            theta += delta_theta;
-        }
-
-        // adjust / prune for model intersection
-        for (auto& candidate : candidates) {
-
-            // if tool intersects model, adjust by reducing radius if possible else remove from set
-            auto tool_intersects_model = [&]() {
-                candidate.generate_tool(current, tool_radius);
-
-                cl::Clipper clipper;
-                clipper.AddPaths(model, cl::ptSubject, true);
-                clipper.AddPaths(candidate.tool, cl::ptClip, true);
-
-                cl::Paths intersection;
-                clipper.Execute(cl::ctIntersection, intersection);
-                return !intersection.empty();
-            };
-
-            if (!tool_intersects_model()) {
-                candidate.generate_engagement(current, tool_radius, stock);
-                auto engagement = candidate.engagement;
-                auto equiv = [&]{
-                    return std::abs(engagement - candidate.engagement) < 0.1;
-                };
-
-                if (false) {
-                    while (engagement > 0 && equiv()) {
-                        std::cerr << "engagement: " << engagement << " radius: " << candidate.r << "\n";
-                        candidate.r += 0.03;
-                        candidate.generate_engagement(current, tool_radius, stock);
-                    }
-                    candidate.r -= 0.03;
-                    // expand while engagement remains approx constant
-                }
-            }
-
-            while (candidate.r > 0 && tool_intersects_model()) {
-                candidate.r -= 0.06;
-            }
-        }
-
-        candidates.erase(std::remove_if(begin(candidates), end(candidates), [](const candidate_t& t) { return t.r <= 0; }), end(candidates));
-
-        // Generate cutter engagements
-        for (auto& candidate : candidates) {
-            candidate.generate_engagement(current, tool_radius, stock);
-        }
-
-        candidates.erase(std::remove_if(begin(candidates), end(candidates), [](const candidate_t& t) { return t.engagement <= 0; }), end(candidates));
-
-        std::sort(begin(candidates), end(candidates), [this](const candidate_t& c0, const candidate_t& c1) {
-            auto cmp = [this](const candidate_t& c) {
-                auto dt = std::abs(c.theta - current_theta);
-                return std::make_tuple(dt < PI/4, std::abs(c.engagement - target_engagement));
-            };
-            return cmp(c0) < cmp(c1);
-        });
-
-        for (auto& candidate : candidates) {
-            std::cerr << "t: " << candidate.theta << " engagement: " << candidate.engagement << " target: " << target_engagement << "\n";
-        }
-
-        if (!candidates.empty()) {
-            auto& candidate = candidates.front();
-            next_point = candidate.generate_point(current);
-            std::cerr << "T: " << candidate.theta << " ENGAGEMENT: " << candidate.engagement << " TARGET: " << target_engagement << "\n";
-
-            cl::Clipper clipper;
-            clipper.AddPaths(stock, cl::ptSubject, true);
-            clipper.AddPaths(candidate.tool, cl::ptClip, true);
-
-            stock.clear();
-            clipper.Execute(cl::ctDifference, stock);
-
-            //output_path(candidate.tool);
-
-            current = next_point;
-            current_theta = candidate.theta;
-            return true;
-        }
-        return false;
-    }
-};
 
 int main(int argc, char* argv[]) {
     po::options_description options("staticcam");
@@ -351,47 +158,132 @@ int main(int argc, char* argv[]) {
         notify(vm);
 
         std::vector<std::vector<point_2>> geometry;
-        geometry.push_back(tool(12.5));   // TODO simple circle
+        geometry.push_back(tool(20));   // TODO simple circle
 
         std::vector<std::vector<point_2>> stock;
         {
             stock.emplace_back();
             auto& path = stock.back();
-            path.push_back({-15, -15});
-            path.push_back({15, -15});
-            path.push_back({15, 15});
-            path.push_back({-15, 15});
+            path.push_back({-30, -30});
+            path.push_back({30, -30});
+            path.push_back({30, 30});
+            path.push_back({-30, 30});
         }
 
         double tool_diameter = 5.0;
 
-        state s;
-        s.current = point_2(-15, -15);
-        s.tool_radius = tool_diameter/ 2.0;
-        s.target_engagement = vm["engagement"].as<double>();
-        s.stock = scale_paths(stock);
-        s.model = scale_paths(geometry);
+        cl::Clipper clipper;
+        clipper.AddPaths(scale_paths(stock), cl::ptSubject, true);
+        clipper.AddPaths(scale_paths(geometry), cl::ptClip, true);
+        cl::Paths material_to_remove;
+        clipper.Execute(cl::ctDifference, material_to_remove);
 
-        output_path(scale_paths(geometry));
-        output_path(s.stock);
+        output_path(material_to_remove);
 
-        point_2 point = s.current;
-        std::cout << "M " << r6(point.x) << " " << r6(point.y) << " ";
-        while (s.next(point)) {
-            std::cout << "L " << r6(point.x) << " " << r6(point.y) << " ";
-            std::cout << std::endl;
+        cl::ClipperOffset co;
+        co.AddPaths(material_to_remove, cl::jtRound, cl::etClosedPolygon);
+        double step = tool_diameter / 2.0;// (180.0 / vm["engagement"].as<double>());
+        double offset = -step;
+        double target_engagement = vm["engagement"].as<double>();
+
+        std::vector<cl::Paths> levels;
+        while (true) {
+            cl::Paths level;
+            co.ArcTolerance = 0.1 * CLIPPER_SCALE;            
+            co.Execute(level, offset * CLIPPER_SCALE);
+
+            if (level.empty())
+                break;
+
+            levels.insert(begin(levels), level);
+
+            offset -= step;
         }
 
+        auto translate = [](const cl::Path& path, cl::IntPoint p) {
+            cl::Path t;
+            for (auto& px : path)
+                t.emplace_back(px.X + p.X, px.Y + p.Y);
+            return t;
+        };
+        auto lerp = [](const cl::IntPoint& P0, const cl::IntPoint& P1, double t) {
+            auto p0 = unscale_point(P0);
+            auto p1 = unscale_point(P1);
+            return scale_point({ (1-t)*p0.x + t*p1.x, (1-t)*p0.y + t*p1.y });
+        };
+        auto distance = [](const point_2& a, const point_2& b) {
+            return std::sqrt(std::pow(b.x - a.x, 2) + std::pow(b.y - a.y, 2));
+        };
+        auto path_length = [](const cl::Paths& paths) {
+            double length = 0;
+            if (paths.empty()) return length;
+            for (auto& path : paths) {
+                if (path.empty()) return length;
+                auto p0 = unscale_point(path.front());
+                for (auto& point : path) {
+                    auto p1 = unscale_point(point);
+                    length += std::sqrt(std::pow(p1.x - p0.x, 2) + std::pow(p1.y - p0.y, 2));
+                    p0 = p1;
+                }
+            }
+            return length;
+        };
 
+        auto tool_model = scale_path(tool(tool_diameter/2.0));
+        // TODO contour offset material to remove!
+        for (auto& level : levels) {
+            for (auto& path : level) {
+                auto pn1 = path.back();
+                //std::cout << "M " << r6(first.x) << " " << r6(first.y) << " ";
+                for (auto& pn : path) {
+                    auto d = distance(unscale_point(pn1), unscale_point(pn));
+                    auto steps = d / .1;
+                    if (steps < 1) steps = 1;
 
-        // Temp simple pocket
+                    auto step = 1.0/steps;
+                    for (double t = 0; t <= 1; t += step) {
+                        // line from pn-1 - pn
+                        auto tp = translate(tool_model, lerp(pn1, pn, t));
 
-        /* agent must output path (list of points)
-         * agent calculates next action, move to point!
-         * how does agent decide how to move (i.e. which parameters are to be optimised?)
-         * */
+                        cl::Clipper clipper;
+                        clipper.AddPaths(material_to_remove, cl::ptClip, true);
+                        clipper.AddPath(tp, cl::ptSubject, false);
 
-        // TODO
+                        cl::PolyTree pt;
+                        clipper.Execute(cl::ctIntersection, pt);
+
+                        auto l0 = path_length({tp});
+                        cl::Paths open;
+                        OpenPathsFromPolyTree(pt, open);
+
+                        double engagement = -1;
+                        if (!open.empty()) {
+                            auto l1 = path_length(open);
+                            engagement = 360.0 * (l1 / l0);
+                            
+                            if (std::abs(engagement - target_engagement) > 10.0) {
+                                //auto dx = pn.x - pn1.x;
+                                //auto dy = pn.y - pn1.y;
+                                //auto p3 = vector_2(-dy, dx);
+
+                            }
+                        }
+                        //std::cout << "L " << r6(pn.x) << " " << r6(pn.y) << " ";
+
+                        {
+                            cl::Clipper clipper;
+                            clipper.AddPaths(material_to_remove, cl::ptSubject, true);
+                            clipper.AddPath(tp, cl::ptClip, true);
+
+                            material_to_remove.clear();
+                            clipper.Execute(cl::ctDifference, material_to_remove);
+                        }
+                    }
+                    pn1 = pn;
+                }
+            }
+        }
+
     } catch(const po::error& e) {
         print_exception(e);
         std::cout << options << "\n";
